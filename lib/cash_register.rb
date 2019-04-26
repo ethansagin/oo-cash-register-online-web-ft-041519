cfc4583cdf8b6455f19e9fe7c6b4ea2d2c73807d
Ethan Sagin <ethansagin@gmail.com>
2019-04-26 00:08:36 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -11,7 +11,7 @@ class CashRegister
   end
 
   def add_item(title, price, quantity = 1)
-    self.total += (price*quantity)
+    total += (price*quantity)
     quantity.times {items << title}
     @last_trans = price
   end
